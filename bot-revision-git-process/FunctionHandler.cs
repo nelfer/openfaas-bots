8b1bd2b197476ca7162db464673553a650688752
Nelson <nelsonr@ngcsoftware.com>
2020-07-13 17:42:02 -0400

    Git to post directly to Jira

--- bot-revision-git-process/FunctionHandler.cs
+++ bot-revision-git-process/FunctionHandler.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Net;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Linq;
+using System.Collections.Generic;
 
 using Newtonsoft.Json;
 
@@ -23,44 +24,65 @@ namespace Function
 			}
 			if(post!=null)
 			{
-				string delimiter="SVNDELIMITERTEXT---- ";
-				string messageFormat="{0}\n{1}\n{2}\n{3}\n{4}\n{5}\n{6}\n{7}\n{8}";
-				post.resource?.commits?.OrderBy(y=>y.author.date).ToList().ForEach(x=>
+				string origMessage=post.detailedMessage.text;
+				origMessage=origMessage.Replace("\r\n","\n").Replace("\r","\n");
+				List<string> msgLines=new List<string>(origMessage.Split('\n'));
+				string fline=msgLines[0];
+				msgLines.RemoveAt(0);
+				string comments="";
+				msgLines.ForEach(x=>comments+=String.Format("{0}\n",x));
+				string fullCommnet=String.Format("{panel:title={0}|borderStyle=dashed|borderColor=#ccc|titleBGColor=#F7D6C1|bgColor=#FFFFCE}\n{1}\n{panel}",fline,comments);
+				
+				List<string> issues=getIssues(origMessage);
+				if(issues.Count>0)
 				{
-					string message=null;
-					try
-					{
-						string branch=post.resource.repository.defaultBranch;
-						branch=branch.Substring(branch.LastIndexOf('/')+1);
-						string repo=String.Format("{0}.{1}",post.resource.repository.name,branch);
-						message=String.Format(messageFormat,
-							repo,
-							delimiter,
-							String.Format("{0}/commit/{1}|{1}",post.resource.repository.remoteUrl,x.commitId),
-							delimiter,
-							x.author.name,
-							delimiter,
-							x.comment,
-							delimiter,
-							""
-						);
-
-						using(WebClient wc=new WebClient())
-						{
-							wc.UploadData("http://gateway:8080/function/bot-revision-process",Encoding.UTF8.GetBytes(message));
-						}
-						System.Console.WriteLine("Posted this: \n{0}",message);
-					}
-					catch(Exception ex)
-					{
-						Console.Write("Failed to process commit: {0}\nMessage Posted:\n {1}",ex.Message,message);
-					}
-				});
+					issues.ForEach(issue=>PostToJira(issue,fullCommnet));
+					System.Console.WriteLine("Posted");
+				}
 			}
 			else
 			{
 				Console.Write("Post was null");
 			}
 		}
+
+		List<string> getIssues(string _comment)
+		{
+			List<string> ret=new List<string>();
+			string pattern = @"[a-zA-Z][a-zA-Z0-9_]+-[1-9][0-9]*";
+			Regex rgx = new Regex(pattern);
+			
+			foreach (Match match in rgx.Matches(_comment))
+			{
+				if(!ret.Contains(match.Value))
+				{
+					ret.Add(match.Value);
+				}
+			}
+			return ret;
+		}
+
+		void PostToJira(string issue,string comment)
+		{
+			try
+			{
+				using(WebClient wc=new WebClient())
+				{
+					Comment cm=new Comment();
+					cm.issue=issue;
+					cm.body=comment;
+					wc.UploadString("http://gateway:8080/function/bot-jira-comment",JsonConvert.SerializeObject(cm));
+				}
+			}
+			catch
+			{
+				// If it fails, it's probably not a real issue, so we don't worry about that
+			}
+		}
+	}
+	public class Comment
+	{
+		public string issue { get; set; }
+		public string body { get; set; }
 	}
 }
--- bot-revision-git-process/tfs-models.cs
+++ bot-revision-git-process/tfs-models.cs
@@ -27,9 +27,18 @@ public class TfsResource
 	public List<TfsGitCommit> commits { get; set; }
 	public TfsRepository repository { get; set; }
 }
+
+public class TsfDetailedMessage
+{
+	public string text { get; set; }
+	public string html { get; set; }
+	public string markdown { get; set; }
+}
+
 public class TfsGitPost
 {
 	public string eventType { get; set; }
 	public TfsResource resource { get; set; }
+	public TsfDetailedMessage detailedMessage { get; set; }
 }
 
